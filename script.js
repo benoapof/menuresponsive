1b304a23f5eae2189ec41c679e5718d95383c0b9
unknown <b.poulard@OFM307.domaine.local>
2014-10-02 17:58:36 +0200

    Ajustement affichage et html + séparation script pour petit et grand écran

--- script.js
+++ script.js
@@ -2,23 +2,59 @@
 // je grossis le trait, le breakpoint est à 1024, c'est un test
 
 // si on retaille la fenêtre
-var doit;
+/*var doit;
 window.onresize = function() {
 	clearTimeout(doit);
 	doit = setTimeout(affLogoBdc, 100);
 };
+*/
 
+// charge un js à la demande
+function callScript(url) {
+	var id = 'js' + url.split('.')[0];
+	if ( document.getElementById(id) === null ) {
+		var s = document.createElement('script');
+		s.id = id;
+		s.type = 'text/javascript';
+		s.async = 'async';
+		s.src = url;
+		document.body.appendChild(s);
+	}
+}
+
+window.onresize = function() {
+	// scripts si la fenêtre est grande
+	if (window.matchMedia("(min-width : 1024px)").matches) {
+		callScript('grand.js');
+//		taille = 'grand';
+	}
+	// scripts si la fenêtre est petite
+	if (window.matchMedia("(max-width : 1023px)").matches) {
+		callScript('petit.js');
+//		taille = 'petit';
+	}
+//console.log	(window.innerWidth+'px / '+taille);
+}
+
+
+/*
 //comportement du logo au scroll
 function affLogoBdc () {
 
 	window.onscroll = function() {
+console.log('gni scroll');
 		var elem = document.getElementsByTagName('h1')[0];
-		if (elem.getBoundingClientRect().top < 40-elem.offsetHeight)
+		if (elem.getBoundingClientRect().top < 40-elem.offsetHeight) {
 			document.getElementsByClassName('logoBdc')[0].style.display = "block";
-		else
+console.log('g\'affiche');
+		}
+		else {
 			document.getElementsByClassName('logoBdc')[0].style.display = "none";
+console.log('g\'affiche p\'us');
+		}
 	};
 
 }
+*/
 
-affLogoBdc();
+/*affLogoBdc();*/
