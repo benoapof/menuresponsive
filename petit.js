5eb3eddba152a907121ce7e7af0f724e90c9d2d1
unknown <b.poulard@OFM307.domaine.local>
2014-10-07 17:41:56 +0200

    Ajustement affichage et début js pour eviter l'ouverture similtané des 2 menus en version mobile + Ajout d'un formulaire en mobile

--- petit.js
+++ petit.js
@@ -1,1 +1,10 @@
 console.log('petit');
+
+document.getElementById('affMenuSite').onclick = function () {
+	console.log('t\'as cliqué sur affMenuSite');
+	document.getElementById('affBDC').checked = false;
+};
+document.getElementById('affBDC').onclick = function () {
+	console.log('t\'as cliqué sur affBDC');
+	document.getElementById('affMenuSite').checked = false;
+};
